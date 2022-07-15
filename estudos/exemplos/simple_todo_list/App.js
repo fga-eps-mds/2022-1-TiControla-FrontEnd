81e27b76be2deff902bf8828a95a67947b724e16
Rodolfo Cabral Neves <roddas360@gmail.com>
2022-07-15 14:32:45 -0300

    Adicionado o cabeçalho da aplicação

--- estudos/exemplos/simple_todo_list/App.js
+++ estudos/exemplos/simple_todo_list/App.js
@@ -1,10 +1,32 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { useState } from 'react';
+import { StyleSheet, Text, View,FlatList } from 'react-native';
+import Header from './components/header';
 
 export default function App() {
+
+  const [todoList, setTodoList] = useState([
+    { task: 'Programar', key: 1 },
+    { task:'Ir pro rolê com o Leo e o Matheus',key : 2},
+    { task: 'Fechar a issue', key: 3 },
+    { task: 'Aprender a fazer Pull Request', key: 4 },
+  ]);
+
   return (
     <View style={styles.container}>
-      
+      <Header />      
+      <View style={styles.content}>
+        
+        {/* to form */}
+        
+        <View style={styles.list} >
+          
+          <FlatList data={todoList} renderItem={({ item })=>(
+            <Text>{item.task}</Text>
+          )}
+          />
+        </View>
+      </View>
     </View>
   );
 }
@@ -13,7 +35,11 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
   },
+  content:{
+    padding:40,
+  },
+  list:{
+    marginTop:20
+  }
 });
