d1f03a21a8dd187d7b5aa5ba0cd2882b26bdf7bc
MaltaLiz <lizlrmalta@gmail.com>
2022-07-20 12:09:40 -0300

    adicionaod icones ao header

--- react-app/ti-controla-app/src/views/Home/components/Header.tsx
+++ react-app/ti-controla-app/src/views/Home/components/Header.tsx
@@ -1,21 +1,26 @@
-import { View, StyleSheet } from "react-native";
-import { usuarioTest } from "../../../application/mocks/usuarioTest";
-import Texto from "../../../components/Texto";
-import Menu from "./Menu";
+import { View, StyleSheet, TouchableOpacity, Image } from 'react-native';
+import { Usuario } from '../../../application/types/Usuario';
+import Texto from '../../../components/Texto';
+import Menu from './Menu';
+import logoutIcon from '../../../assets/icons/logout.png';
+import profileIcon from '../../../assets/icons/profile.png';
+import { dimensao } from '../../../utils/dimensoesDoDipositivo';
 
-export default function Header() {
+export default function Header({nome, saldo}: Usuario) {
   return (
-    <View style={{ position: "relative"}}>
+    <View style={{ position: 'relative'}}>
       <View style={estilos.container}>
-        <Texto tipo="negrito" style={estilos.texto}>
-          Olá, {usuarioTest.nome}
+        <Texto tipo='negrito' style={estilos.texto}>
+          Olá, {nome}
         </Texto>
-        <Texto tipo="medio" style={estilos.texto}>
-          {usuarioTest.saldo}
+        <Texto tipo='medio' style={estilos.texto}>
+          {saldo}
         </Texto>
         <View style={estilos.menu}>
           <Menu/>
         </View>
+        <TouchableOpacity style={estilos.lougoutIcon}><Image source={logoutIcon} width={32} height={32}/></TouchableOpacity>
+        <TouchableOpacity style={estilos.profileIcon}><Image source={profileIcon} width={32} height={32}/></TouchableOpacity>
       </View>
     </View>
   );
@@ -24,19 +29,33 @@ export default function Header() {
 const estilos = StyleSheet.create({
   container: {
     paddingVertical: 70,
-    textAlign: "center",
-    backgroundColor: "#0B4B54",
-    color: "#fff",
-    width: "100%",
+    textAlign: 'center',
+    backgroundColor: '#0B4B54',
+    color: '#fff',
+    width: dimensao.largura,
+    alignItems: 'center',
+    zIndex: 3
   },
   texto: {
-    color: "#fff",
+    color: '#fff',
     fontSize: 22,
   },
   menu: {
-    position: "absolute",
+    position: 'absolute',
     top: 167,
     width: '100%',
     zIndex: 1,
   },
+  lougoutIcon: {
+    position: 'absolute',
+    zIndex: 2,
+    top: 32,
+    left: 16,
+  },
+  profileIcon: {
+    position: 'absolute',
+    zIndex: 2,
+    top: 32,
+    right: 16,
+  }
 });
