605cf173dbddd64eca3d7349e3de2db2a98e4656
MaltaLiz <lizlrmalta@gmail.com>
2022-07-20 00:18:53 -0300

    exemplificando o uso dos tipos de texto

--- react-app/ti-controla-app/App.tsx
+++ react-app/ti-controla-app/App.tsx
@@ -1,16 +1,31 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
-
+import { StyleSheet, Text, SafeAreaView } from 'react-native';
+import {useFonts, Quicksand_400Regular, Quicksand_500Medium, Quicksand_700Bold} from '@expo-google-fonts/quicksand';
+import AppLoading from 'expo-app-loading';
+import Texto from './src/components/Texto'
+ 
 export default function App() {
+  const[fonteCarregada] = useFonts({
+    'QuicksandRegular': Quicksand_400Regular,
+    'QuicksandMedium': Quicksand_500Medium,
+    'QuicksandBold': Quicksand_700Bold,
+  })
+
+  if(!fonteCarregada) return <AppLoading/>
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
+    <SafeAreaView style={styles.container}>
+      <Texto>Texto normal</Texto>
+      <Texto tipo='medio'>Texto medio</Texto>
+      <Texto tipo='negrito'>Texto negrito</Texto>
       <StatusBar style="auto" />
-    </View>
+    </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
+  texto: {
+  },
   container: {
     flex: 1,
     backgroundColor: '#fff',
