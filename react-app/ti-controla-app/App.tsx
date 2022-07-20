982f868cdf53304685cd1ec3bf3c564f4a5f1de5
MaltaLiz <lizlrmalta@gmail.com>
2022-07-20 00:58:34 -0300

    exemplificando como usar o componente botao

--- react-app/ti-controla-app/App.tsx
+++ react-app/ti-controla-app/App.tsx
@@ -3,6 +3,7 @@ import { StyleSheet, Text, SafeAreaView } from 'react-native';
 import {useFonts, Quicksand_400Regular, Quicksand_500Medium, Quicksand_700Bold} from '@expo-google-fonts/quicksand';
 import AppLoading from 'expo-app-loading';
 import Texto from './src/components/Texto'
+import Botao from './src/components/Botao';
  
 export default function App() {
   const[fonteCarregada] = useFonts({
@@ -18,18 +19,21 @@ export default function App() {
       <Texto>Texto normal</Texto>
       <Texto tipo='medio'>Texto medio</Texto>
       <Texto tipo='negrito'>Texto negrito</Texto>
+      <Botao tipo='grande' cor='verde'>Entrar</Botao>
+      <Botao tipo='pequeno' cor='verde'>Editar</Botao>
+      <Botao tipo='grande' cor='vermelho'>Excluir minha conta</Botao>
+      <Botao tipo='pequeno' cor='vermelho'>Deletar</Botao>
       <StatusBar style="auto" />
     </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
-  texto: {
-  },
   container: {
     flex: 1,
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+    gap: 10,
   },
 });
