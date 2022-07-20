f49aa640e6d802524ae5f17a9d592b6c986d3521
MaltaLiz <lizlrmalta@gmail.com>
2022-07-20 12:10:58 -0300

    acrescentando componentes na home e usando props

--- react-app/ti-controla-app/src/views/Home/index.tsx
+++ react-app/ti-controla-app/src/views/Home/index.tsx
@@ -1,8 +1,52 @@
+import { usuarioTest } from "../../application/mocks/usuarioTest";
+import InfoContainer from "../../components/InfoContainer";
 import Header from "./components/Header";
-import Menu from "./components/Menu";
+import { View, StyleSheet } from "react-native";
+import Texto from "../../components/Texto";
+import { Usuario } from "../../application/types/Usuario";
 
-export default function Home() {
-    return <>
-        <Header/>
-    </>
+export default function Home({
+  id,
+  nome,
+  email,
+  saldo,
+  limiteDisponivel,
+  limiteMaximo,
+  totalDasFaturas,
+}: Usuario) {
+  return (
+    <View style={{ alignItems: "center" }}>
+      <Header {...usuarioTest} />
+      <InfoContainer style={estilos.container}>
+        <View>
+          <Texto style={estilos.tituloInfo}>Total das faturas:</Texto>
+          <Texto style={estilos.valorInfo}>{totalDasFaturas}</Texto>
+        </View>
+        <View>
+          <Texto style={estilos.tituloInfo}>
+            Seu limite de crédito mensal:
+          </Texto>
+          <Texto style={estilos.valorInfo}>{limiteMaximo}</Texto>
+        </View>
+        <View>
+          <Texto style={estilos.tituloInfo}>Seu limite de crédito hoje:</Texto>
+          <Texto style={estilos.valorInfo}>{limiteDisponivel}</Texto>
+        </View>
+      </InfoContainer>
+    </View>
+  );
 }
+
+const estilos = StyleSheet.create({
+  container: {
+    marginTop: 69,
+  },
+  tituloInfo: {
+    color: "#fff",
+    fontSize: 16,
+  },
+  valorInfo: {
+    color: "#fff",
+    fontSize: 22,
+  },
+});
