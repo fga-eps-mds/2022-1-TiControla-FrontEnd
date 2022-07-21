db0b2f4ef7b17b790669dcc6a6777fef8108171d
Rodolfo Cabral Neves <roddas360@gmail.com>
2022-07-21 03:52:56 -0300

    Melhorando a UI/UX do usuário, adicionando icones a cada elemento da lista

--- estudos/exemplos/simple_todo_list/components/todoItems.js
+++ estudos/exemplos/simple_todo_list/components/todoItems.js
@@ -1,10 +1,14 @@
-import { StyleSheet, Text, TextInput, TouchableOpacity } from 'react-native';
+import { StyleSheet, Text, View, TouchableOpacity } from 'react-native';
+import {MaterialIcons} from '@expo/vector-icons';
 
 export default function TodoItem({ item, pressHandler })
 {
     return(
         <TouchableOpacity onPress={() => pressHandler(item.key)}>
-            <Text style={styles.item}>{item.task}</Text>
+            <View style={styles.item}>
+                <MaterialIcons name='delete' size={18} color='#333' />
+                <Text style={styles.itemText}>{item.task}</Text>
+            </View>
         </TouchableOpacity>
     );
 }
@@ -18,6 +22,10 @@ const styles = StyleSheet.create(
             borderColor:'#bbb',
             borderStyle:'dashed',
             borderRadius:10,
+            flexDirection:'row'
+        },
+        itemText:{
+            marginLeft:10
         }
     }
 );
