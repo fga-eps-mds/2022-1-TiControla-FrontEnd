b3b8dd47361a0378fb9683bd130ea867f29dbab1
hgaldino05 <hgaldino05@gmail.com>
2022-07-21 21:59:27 -0300

    falta ajustar o ícone usuário e criar o modal

--- react-app/ti-controla-app/App.tsx
+++ react-app/ti-controla-app/App.tsx
@@ -1,9 +1,11 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, SafeAreaView } from 'react-native';
+import { StyleSheet, Text, SafeAreaView, View } from 'react-native';
 import {useFonts, Quicksand_400Regular, Quicksand_500Medium, Quicksand_700Bold} from '@expo-google-fonts/quicksand';
 import AppLoading from 'expo-app-loading';
 import Texto from './src/components/Texto'
 import Botao from './src/components/Botao';
+import React from 'react';
+import Header from './src/components/Header';
  
 export default function App() {
   const[fonteCarregada] = useFonts({
@@ -16,24 +18,63 @@ export default function App() {
 
   return (
     <SafeAreaView style={styles.container}>
-      <Texto>Texto normal</Texto>
-      <Texto tipo='medio'>Texto medio</Texto>
-      <Texto tipo='negrito'>Texto negrito</Texto>
-      <Botao tipo='grande' cor='verde'>Entrar</Botao>
-      <Botao tipo='pequeno' cor='verde'>Editar</Botao>
-      <Botao tipo='grande' cor='vermelho'>Excluir minha conta</Botao>
-      <Botao tipo='pequeno' cor='vermelho'>Deletar</Botao>
+      
+      <Header/>
+      
+        <View style={styles.textos}>
+
+          
+          
+
+          <Texto tipo='negrito'>Telefone: </Texto>
+          <Texto tipo='medio'>4002-8922</Texto>
+
+          <Texto tipo='negrito'>Email: </Texto>
+          <Texto tipo='medio'>joao@gmail.com</Texto>
+
+          <Texto tipo='negrito'>Limite de crédito: </Texto>
+          <Texto tipo='medio'>R$ 5499,99</Texto>
+
+        </View>
+
+        <View style={styles.botoes}>
+
+          <Botao tipo='grande' cor='verde'>Alterar limite de crédito</Botao>
+
+        </View>
+
       <StatusBar style="auto" />
     </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
+  
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
+    flexDirection: 'column',
     justifyContent: 'center',
-    gap: 10,
+    alignContent: 'center',
+    backgroundColor: '#fff',
+    
+
   },
+  botoes: {
+    flex: 1,
+    flexDirection: 'column',
+    justifyContent: 'center',
+    alignContent: 'center',
+    backgroundColor: '#fff',
+    marginBottom: 60,
+    marginHorizontal: 60,
+
+  },
+  textos: {
+    flex: 1,
+    flexDirection: 'column',
+    position: 'relative',
+    justifyContent: 'space-evenly',
+    backgroundColor: '#fff',
+    marginLeft: 16,
+  }
 });
