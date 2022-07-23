38092518b7ac1e3bdd9b0a5bec37c881fa22f126
MaltaLiz <lizlrmalta@gmail.com>
2022-07-23 11:08:32 -0300

    criando telas de lançamentos de credito e suas subtelas

---
+++ react-app/ti-controla-app/src/views/creditoLancamentos/views/edicaoLancamentoCredito/index.tsx
@@ -0,0 +1,8 @@
+import { RootStackScreenProps } from '../../../../application/types/RootStack';
+export default function EdicaoLancamentoCredito({ navigation, route }: RootStackScreenProps<"EdicaoLancamentoCredito">) {
+  const {lancamento} = route.params;
+  
+  return <>
+    
+  </>
+}
