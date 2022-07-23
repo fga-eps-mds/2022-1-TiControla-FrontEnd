9f24b22d68fe77c4603c50a29dd3e5018e2a63ad
MaltaLiz <lizlrmalta@gmail.com>
2022-07-23 02:08:44 -0300

    adicionando tela cadastro na navegacao

--- react-app/ti-controla-app/src/application/navigation/index.tsx
+++ react-app/ti-controla-app/src/application/navigation/index.tsx
@@ -1,5 +1,6 @@
 import {NavigationContainer} from '@react-navigation/native'
 import {createNativeStackNavigator} from '@react-navigation/native-stack'
+import Cadastro from '../../views/cadastro';
 import Home from '../../views/home';
 import Login from '../../views/login';
 import { RootStackParamList } from '../types/RootStack';
@@ -19,6 +20,7 @@ function RootNavigator(){
     <Stack.Navigator>
         <Stack.Screen name='Login' component={Login} options={{headerShown: false}}/>
         <Stack.Screen name='Home' component={Home} options={{headerShown: false}}/>
+        <Stack.Screen name='Cadastro' component={Cadastro} options={{headerShown: false}}/>
     </Stack.Navigator>
     )
 }
