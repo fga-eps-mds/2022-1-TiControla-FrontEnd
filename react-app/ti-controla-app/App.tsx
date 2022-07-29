80a18b4d5367ba9f81eedca687e53323d903bcc6
Rodolfo Cabral Neves <roddas360@gmail.com>
2022-07-28 22:36:18 -0300

    Adicionado o componente DebtRegistration, que é a página de visualização do cadastramento de débito

--- react-app/ti-controla-app/App.tsx
+++ react-app/ti-controla-app/App.tsx
@@ -1,11 +1,6 @@
-import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, SafeAreaView, View } from 'react-native';
 import {useFonts, Quicksand_400Regular, Quicksand_500Medium, Quicksand_700Bold} from '@expo-google-fonts/quicksand';
-import Texto from './src/components/Texto'
-import Botao from './src/components/Botao';
-import { usuarioTest } from './src/application/mocks/usuarioTest';
-import React from 'react';
-import Profile from './src/views/Profile';
+import { DebtRegistration } from './src/views/DebtRegistration';
  
 export default function App() {
   const[fonteCarregada] = useFonts({
@@ -26,15 +21,7 @@ export default function App() {
   });
   return (
     <SafeAreaView style={styles.container}>
-      {/* <Texto>Texto normal</Texto>
-      <Texto tipo='medio'>Texto medio</Texto>
-      <Texto tipo='negrito'>Texto negrito</Texto>
-      <Botao tipo='grande' cor='verde'>Entrar</Botao>
-      <Botao tipo='pequeno' cor='verde'>Editar</Botao>
-      <Botao tipo='grande' cor='vermelho'>Excluir minha conta</Botao>
-      <Botao tipo='pequeno' cor='vermelho'>Deletar</Botao> */}
-      
-      <Profile{...usuarioTest}/>
+          <DebtRegistration />
     </SafeAreaView>
   );
 }
