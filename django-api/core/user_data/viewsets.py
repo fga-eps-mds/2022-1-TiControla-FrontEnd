78899b1a47930d4085a0aaa141e9c1a06c328f39
Leonardo M. Miranda <leo@alephzero.ai>
2022-07-31 09:15:34 -0300

    usar msgs de erro apropriadas na django-api
    Caso o usuario nao esteja autenticado quando requisitar user_data, retornar o erro HTTP_403_FORBIDDEN. Melhorar a segurança desse request.

--- django-api/core/user_data/viewsets.py
+++ django-api/core/user_data/viewsets.py
@@ -1,20 +1,20 @@
 # from rest_framework import viewsets
 from user_data import serializers
 from user_data import models
-from rest_framework import generics
+from rest_framework import generics, status
+from django.contrib.auth import get_user
 
-
-# class UserDataViewSet(viewsets.ModelViewSet):
-#     serializer_class = serializers.UserSerializer
-#     queryset = models.UserData.objects.all()
 
 # classe para pegar os dados do usuario, requer que o usuario esteja autenticado
 class UserDataView(generics.RetrieveAPIView):
     serializer_class = serializers.UserDataSerializer
 
     def get_object(self):
-        if not self.request.user.is_authenticated:
-            pass  # Do something for anonymous users.
-            return self.request.user
-        # Do something for logged-in users.
+        # check if user is authenticated
+        if (not self.request.user.is_authenticated) or (not get_user(self.request).is_authenticated):
+            return Response(None, status=status.HTTP_403_FORBIDDEN)
+        # check if the request username belongs to the user behind it
+        if self.request.user.username != get_user(self.request).username:
+            return Response(None, status=status.HTTP_403_FORBIDDEN)
+        # returns the data that belongs to the user
         return models.UserData.objects.get(username=self.request.user.username)
