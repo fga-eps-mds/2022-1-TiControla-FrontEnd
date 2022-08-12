21a63551a12146980893e21646e8e5a93913018f
Leonardo M. Miranda <leonardomichalskim@gmail.com>
2022-08-12 15:11:30 -0300

    #46 #48 Deploy contínuo do cluster k8s e github secrets

--- src/core/settings.py
+++ src/core/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,10 +21,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-#+ax)k%&en_31$8s-*yyb^#*t3u4007(5=xi(q168rxxyt0tc('
+SECRET_KEY = os.getenv("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 # SECURITY WARNING: nao permitir todos os hosts na producao
 ALLOWED_HOSTS = ['*']
