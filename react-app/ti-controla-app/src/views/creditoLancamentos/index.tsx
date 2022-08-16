d082c1200a1600e72f64e2046aa2e30bbb116eab
MaltaLiz <lizlrmalta@gmail.com>
2022-08-16 14:12:09 -0300

    #59 tirando botao desnecessario

--- react-app/ti-controla-app/src/views/creditoLancamentos/index.tsx
+++ react-app/ti-controla-app/src/views/creditoLancamentos/index.tsx
@@ -10,6 +10,5 @@ export default function CreditoLancamentos({ navigation, route }: RootStackScree
   return <>
     <Botao cor="verde" tipo="grande" onPress={() => navigation.navigate('CreditoDetalhamento', {lancamento: lancamentosTeste[2]} )}>Credito detalhamento</Botao>
     <Botao cor="verde" tipo="grande" onPress={() => navigation.navigate("CadastroLancamentoCredito", {id: id})}>Cadastrar novo lançamento</Botao>
-    <Botao cor="verde" tipo="grande" onPress={() => navigation.navigate("EdicaoLancamentoCredito", {lancamento: lancamentosTeste[0]})}>Cadastrar novo lançamento</Botao>
   </>
 }
